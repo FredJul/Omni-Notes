58564376b18d9717616c8ef834186aac09a31236
Frédéric Julian <frederic.julian@doro.com>
2016-02-24 00:04:10 +0100

    fix non visible empty view in some cases

--- TaskGame/src/main/java/net/fred/taskgame/view/EmptyRecyclerView.java
+++ TaskGame/src/main/java/net/fred/taskgame/view/EmptyRecyclerView.java
@@ -10,8 +10,31 @@ import android.view.View;
 import net.fred.taskgame.utils.UiUtils;
 
 public class EmptyRecyclerView extends RecyclerView {
+
+    private final
+    @NonNull
+    AdapterDataObserver mObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+            checkIfEmpty();
+        }
+    };
+
     @Nullable
-    View mEmptyView;
+    private View mEmptyView;
 
     public EmptyRecyclerView(Context context) {
         super(context);
@@ -26,40 +49,46 @@ public class EmptyRecyclerView extends RecyclerView {
     }
 
     @Override
-    public int getMinFlingVelocity() {
-        return UiUtils.dpToPixel(500); // Nothing related to empty view, but the default velocity is clearly TOO sensible!
-    }
-
-    void checkIfEmpty() {
-        if (mEmptyView != null) {
-            mEmptyView.setVisibility(getAdapter().getItemCount() > 0 ? GONE : VISIBLE);
-        }
-    }
-
-    final
-    @NonNull
-    AdapterDataObserver observer = new AdapterDataObserver() {
-        @Override
-        public void onChanged() {
-            super.onChanged();
-            checkIfEmpty();
-        }
-    };
-
-    @Override
     public void setAdapter(@Nullable Adapter adapter) {
         final Adapter oldAdapter = getAdapter();
         if (oldAdapter != null) {
-            oldAdapter.unregisterAdapterDataObserver(observer);
+            oldAdapter.unregisterAdapterDataObserver(mObserver);
+        }
+
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mObserver);
         }
         super.setAdapter(adapter);
-        if (adapter != null) {
-            adapter.registerAdapterDataObserver(observer);
+        checkIfEmpty();
+    }
+
+    @Override
+    public void swapAdapter(Adapter adapter, boolean removeAndRecycleExistingViews) {
+        final Adapter oldAdapter = getAdapter();
+        if (oldAdapter != null) {
+            oldAdapter.unregisterAdapterDataObserver(mObserver);
         }
+
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mObserver);
+        }
+        super.swapAdapter(adapter, removeAndRecycleExistingViews);
+        checkIfEmpty();
     }
 
     public void setEmptyView(@Nullable View mEmptyView) {
         this.mEmptyView = mEmptyView;
         checkIfEmpty();
+    }
+
+    @Override
+    public int getMinFlingVelocity() {
+        return UiUtils.dpToPixel(500); // Nothing related to empty view, but the default velocity is clearly TOO sensible!
+    }
+
+    private void checkIfEmpty() {
+        if (mEmptyView != null && getAdapter() != null) {
+            mEmptyView.setVisibility(getAdapter().getItemCount() > 0 ? GONE : VISIBLE);
+        }
     }
 }
